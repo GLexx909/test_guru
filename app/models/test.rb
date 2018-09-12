79cccf25aab85020d7f41b5315ef6099d7b01c77
Vitaliy Simonov <guardian909@rambler.ru>
2018-09-12 17:12:45 +0300

    ER

--- app/models/test.rb
+++ app/models/test.rb
@@ -1,5 +1,8 @@
 class Test < ApplicationRecord
   belongs_to :category
+  has_many :questions
+  has_many :test_passages
+  has_many :users, through: :test_passages
 
   def self.titles_by_category(category_title)
     self.joins(:category).where(categories: {title: category_title}).order(title: :DESC).pluck(:title)
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,8 @@
 class User < ApplicationRecord
+  has_many :test_passages
+  has_many :tests, through: :test_passages
 
   def passed_test(level)
-    Test.joins("INNER JOIN test_passages").where(test_passages: {user_id: self.id}).where("level = ?", level).distinct
+    Test.joins(:test_passages).where(test_passages: {user_id: self.id}).where("level = ?", level).distinct
   end
 end
