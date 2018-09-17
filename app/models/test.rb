a8153d18c5af96314a68572fd7cddb8be59fb06d
Vitaliy Simonov <guardian909@rambler.ru>
2018-09-17 09:40:17 +0300

    Fix Test validation

--- app/models/test.rb
+++ app/models/test.rb
@@ -5,10 +5,9 @@ class Test < ApplicationRecord
   has_many :test_passages
   has_many :users, through: :test_passages
 
-  validates :title, presence: true, uniqueness: true
+  validates :title, presence: true, uniqueness: {scope: :level}
   validates :level, numericality: {
     only_integer: true, greater_than_or_equal_to: 0 }
-  validate :level_and_title_unique, on: :create
 
   scope :easy, -> { where(level: 0..1) }
   scope :averages, -> { where(level: 2..4) }
@@ -22,13 +21,5 @@ class Test < ApplicationRecord
   def self.titles_by_category(category_title)
     by_category(category_title).pluck(:title)
   end
-
-  private
-  #Удалю за ненадобностью. Потом.
-  # def level_and_title_unique
-  #   message = "Тест не уникален. Название и уровень совпадают с уже имеющимся."
-  #   test_not_unique = Test.where(level: self.level, title: self.title).any?
-  #   errors.add(:level_and_title_unique, message) if test_not_unique
-  # end
 
 end
--- db/migrate/20180917063043_add_index_to_tests.rb
+++ db/migrate/20180917063043_add_index_to_tests.rb
@@ -1,5 +1,5 @@
 class AddIndexToTests < ActiveRecord::Migration[5.2]
   def change
-    add_index :tests, :title, unique: true
+    add_index :tests, [:title, :level], unique: true
   end
 end
