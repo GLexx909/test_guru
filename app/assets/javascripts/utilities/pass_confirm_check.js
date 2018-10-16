30c5aaf0548d12689733b2b9488ceec6a51595f5
Vitaliy Simonov <guardian909@rambler.ru>
2018-10-16 12:18:10 +0300

    Edit pass_confirm_check.js

--- app/assets/javascripts/utilities/pass_confirm_check.js
+++ app/assets/javascripts/utilities/pass_confirm_check.js
@@ -1,14 +1,14 @@
 document.addEventListener('turbolinks:load', function() {
-  var pass_control = document.querySelector('#pass');
-  var passConfirm_control = document.querySelector('#pass_confirm');
+  var passControl = document.querySelector('#pass');
+  var passConfirmControl = document.querySelector('#pass_confirm');
 
-  if (pass_control && passConfirm_control) { passConfirm_control.addEventListener('keypress', comparePasses) };
+  if (passControl && passConfirmControl) { passConfirmControl.addEventListener('keypress', comparePasses) };
 });
 
 function passConfValueDefinition(event) {
-  var pass_conf = document.querySelector('#pass_confirm').value;
+  var passConfirm = document.querySelector('#pass_confirm').value;
 
-  return pass_conf = pass_conf + String.fromCharCode(event.keyCode);
+  return passConfirm = passConfirm + String.fromCharCode(event.keyCode);
 };
 
 function comparePasses() {
