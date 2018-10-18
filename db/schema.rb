5d9904d6bdb293ec55c6f083bd88dc6a58cbfd59
Vitaliy Simonov <guardian909@rambler.ru>
2018-10-18 19:35:22 +0300

    1935

--- db/schema.rb
+++ db/schema.rb
@@ -12,6 +12,9 @@
 
 ActiveRecord::Schema.define(version: 2018_10_12_135743) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "answers", force: :cascade do |t|
     t.text "body", null: false
     t.boolean "correct", default: false, null: false
