70ce50756ece413beb4249905ee912985499d402
Vitaliy Simonov <guardian909@rambler.ru>
2018-10-19 18:36:12 +0300

    mail config

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -66,11 +66,15 @@ Rails.application.configure do
   config.action_mailer.perform_caching = false
   config.action_mailer.default_url_options = { host: 'fathomless-citadel-14313.herokuapp.com' }
   config.action_mailer.delivery_method = :smtp
+  config.action_mailer.perform_deliveries = true
+  config.action_mailer.raise_delivery_errors = false
+  config.action_mailer.default :charset => "utf-8"
   config.action_mailer.smtp_settings = {
     address:              'smtp.gmail.com',
     port:                 587,
-    user_name:            ENV['SMTP_USERNAME'],
-    password:             ENV['SMTP_PASSWORD'],
+    domain: "fathomless-citadel-14313.herokuapp.com",
+    user_name:            ENV['GMAIL_USERNAME'],
+    password:             ENV['GMAIL_PASSWORD'],
     authentication:       'plain',
     enable_starttls_auto: true
   }
