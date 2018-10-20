5245972025fb337a04ab79a8a9cb7547b5ac4992
Vitaliy Simonov <guardian909@rambler.ru>
2018-10-20 15:59:44 +0300

    layout nav contact_us, flash

--- app/controllers/contact_us_controller.rb
+++ app/controllers/contact_us_controller.rb
@@ -7,8 +7,13 @@ class ContactUsController < ApplicationController
     message = params[:body]
     user = current_user
 
-    ContactUsMailer.send_mail(user, message).deliver_now
-    redirect_to tests_path
+    if ContactUsMailer.send_mail(user, message).deliver_now #Такая схема не работает, конечно)))) Всмысле в случае ошибки.
+      redirect_to tests_path
+      flash[:notice] = t('.success')
+    else
+      render 'show_form'
+      flash[:alert] = t('.failure')
+    end
   end
 
 end
